0324eb225fccb2cbba3ea0f268b5250bd1bf4f4e
Naeem Ahmed <naeem.ahmed@gmail.com>
2016-04-23 12:28:52 +0800

    script updates

--- scripts/bulldozer.py
+++ scripts/bulldozer.py
@@ -3,8 +3,12 @@ import mcpi.block as block
 import time
 mc = minecraft.Minecraft.create()
 
+def bulldoze(pos_x, pos_z):
+    for x in range(-10,10):
+        for z in range(-10,10):
+            for y in range(0,20):
+                mc.setBlock(pos_x + x, 0 + y, pos_z + z, block.AIR.id)
+
 player_pos = mc.player.getTilePos()
-for x in range(-10,10):
-    for z in range(-10,10):
-        for y in range(0,10):
-            mc.setBlock(player_pos.x+x, player_pos.y+y, player_pos.z+z, block.AIR.id)
+
+bulldoze(player_pos.x + 10, player_pos.y + 10)
