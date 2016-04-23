397c1a3c0ba2375f2615bba7a3e45201d58cb008
lucwastiaux <luc.wastiaux@airpost.net>
2016-04-23 08:32:43 +0000

    added fence led

---
+++ scripts/fence_led.py
@@ -0,0 +1,31 @@
+import RPi.GPIO as GPIO
+import mcpi.minecraft as minecraft
+import time
+mc = minecraft.Minecraft.create()
+
+
+LED = 17
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(LED, GPIO.OUT)  # setup for output
+
+led_status = False
+
+z_max = 11
+z_min = 6
+x_max = 61
+x_min = 55
+
+
+while True:
+    time.sleep(0.25)
+    pos = mc.player.getTilePos()
+    inside_fence = False
+    if pos.z > z_min and pos.z < z_max:
+        if pos.x > x_min and pos.x < x_max:
+            inside_fence = True
+    if inside_fence != led_status:
+        led_status = inside_fence
+        GPIO.output(LED, led_status)
+        
+
+ 
